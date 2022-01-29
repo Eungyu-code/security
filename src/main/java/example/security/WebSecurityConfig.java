4051defbcf24c37031ed57bc2c180e53189e5610
LeeEunGyu <agyu625@gmail.com>
2022-01-29 16:04:59 +0900

    passwordEncoder()

--- src/main/java/example/security/WebSecurityConfig.java
+++ src/main/java/example/security/WebSecurityConfig.java
@@ -51,7 +51,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder;
+        return new BCryptPasswordEncoder();
     }
 
 }
